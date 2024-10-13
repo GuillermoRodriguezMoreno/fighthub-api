cc62994fa8f7dfd9f89e3833123692d558001c3f
guille <guille@ejemplo.es>
2024-10-13 12:51:43 +0200

    first commit

---
+++ fighthub-api/src/main/java/com/fighthub/fighthubapi/FighthubApiApplication.java
@@ -0,0 +1,13 @@
+package com.fighthub.fighthubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FighthubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FighthubApiApplication.class, args);
+	}
+
+}
---
+++ fighthub-api/src/test/java/com/fighthub/fighthubapi/FighthubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fighthub.fighthubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighthubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
